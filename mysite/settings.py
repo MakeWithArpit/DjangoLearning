0c3624f6ceb5487d99513e7149154d5a1543ff18
Arpit <arpit.gangwar061@gmail.com>
2025-10-14 21:30:15 +0530

    update credintials of mysqldb on settings.py ---> Databases

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,8 +57,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "mysitedb",
+        "USER": "root",
+        "PASSWORD": "UP25-9904399501",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
