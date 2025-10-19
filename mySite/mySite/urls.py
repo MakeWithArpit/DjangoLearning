78413853e779bb105a4c993c774d05fde3c48ef6
Arpit <arpit.gangwar061@gmail.com>
2025-10-19 21:29:37 +0530

    ...

--- mySite/mySite/urls.py
+++ mySite/mySite/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page),
     path('about/', views.about_us),
-    path('about/<int:num>', views.in_about_us), #dynamic url pattern. access via about/1 or about/2
+    path('about/<int:num>', views.in_about_us), #dynamic url pattern. access via about/1 or about/2. in place of <int:num you can use <str:name> or <slug:slugname> etc based on your requirement
 ]
