baa0dbd90f5c0c9516bc4cc2b35b29c0e59a0c6c
Arpit <arpit.gangwar061@gmail.com>
2025-10-19 20:15:22 +0530

    ...... newww project

--- app/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import home_view
-
-urlpatterns = [
-    path('', home_view, name='home'),
-]
--- app/home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def home_view(request):
-    return HttpResponse("Welcome to the Home Page!")
