ca68ffbee1bc264f2ab1ec92786f83c3e51655f5
Arpit <arpit.gangwar061@gmail.com>
2025-10-19 20:31:58 +0530

    add home page

--- mySite/mySite/views.py
+++ mySite/mySite/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def about_us(request):
     return HttpResponse("This is the About Us page of the website.")
+
+def home_page(request):
+    return HttpResponse("This is the Home page of the website.")
