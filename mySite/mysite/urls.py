1baf81b7ea71e9c71c27769532bfda7732181ce7
Arpit <arpit.gangwar061@gmail.com>
2025-11-03 22:17:53 +0530

    a

--- mySite/mysite/urls.py
+++ mySite/mysite/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from a_home.views import *
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path ('', include('a_home.urls')),
 ]
