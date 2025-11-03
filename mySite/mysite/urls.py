3fc7a606d709d1d222bf241f7a1a73747e195a27
Arpit <arpit.gangwar061@gmail.com>
2025-11-03 21:50:05 +0530

    a

--- mySite/mysite/urls.py
+++ mySite/mysite/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from project_admin.views import views as project_admin_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', project_admin_views.home, name='home'),
 ]
