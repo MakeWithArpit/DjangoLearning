a441f50a2e0cd5a4d82ab2c4c3d6f0f7a242dd6e
Arpit <arpit.gangwar061@gmail.com>
2025-11-03 22:09:52 +0530

    a

--- mySite/mysite/settings.py
+++ mySite/mysite/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project_admin',
+    'a_home',
+    'b_project_admin'
 ]
 
 MIDDLEWARE = [
--- mySite/mysite/urls.py
+++ mySite/mysite/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from home.views import *
+from a_home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
