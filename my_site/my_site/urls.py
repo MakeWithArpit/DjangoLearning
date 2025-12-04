6eac52e21997ac909b67013f3337bce614c33964
Arpit <arpit.gangwar061@gmail.com>
2025-12-04 21:03:57 +0530

    Modified: y_site/authenticationn/templates/index.html, my_site/my_site/__pycache__/urls.cpython-313.pyc, my_site/my_site/urls.py

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from authenticationn.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', auth_home, name='auth_home'),
     path('login', auth_login, name='auth_login'),
+    path('admin/', admin.site.urls),
 ]
