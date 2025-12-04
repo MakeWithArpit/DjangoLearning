6ab6c31bfe409f0f2c40fcd9cc58c15b9483cc5d
Arpit <arpit.gangwar061@gmail.com>
2025-12-04 20:53:50 +0530

    Updated 4 files on 2025-12-04 20:53:50

--- my_site/authenticationn/views.py
+++ my_site/authenticationn/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def auth_home(request):
-    return render(request, 'index.html')
+    data = {
+        "title" : "Authentication Home",
+    }
+    return render(request, 'index.html', data)
 
 def auth_login(request):
     return render(request, '1login.html')
