77a5a72f45b87ca0c205a0edf0df6b6c5e1cdd72
Arpit <arpit.gangwar061@gmail.com>
2025-12-04 18:16:17 +0530

    Updated y_site/authenticationn/views.py

--- my_site/authenticationn/views.py
+++ my_site/authenticationn/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Welcome to the authentication home page!")
 
+def login(request):
+    return render(request, 'login.html')
+
 # Create your views here.
