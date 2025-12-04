0583a07e108f897a770305eb34ad7e06537babb9
Arpit <arpit.gangwar061@gmail.com>
2025-12-04 21:14:02 +0530

    Modified: y_site/authenticationn/__pycache__/views.cpython-313.pyc, my_site/authenticationn/templates/index.html, my_site/authenticationn/views.py

--- my_site/authenticationn/views.py
+++ my_site/authenticationn/views.py
@@ -4,6 +4,14 @@ from django.http import HttpResponse
 def auth_home(request):
     data = {
         "title" : "Authentication Home",
+        "course_list" : ["Django", "Flask", "FastAPI", "Tornado"],
+
+        "student_list" : [
+            {"name": "Alice", "age": 24},
+            {"name": "Bob", "age": 22},
+            {"name": "Charlie", "age": 23},
+        ],
+        
     }
     return render(request, 'index.html', data)
 
