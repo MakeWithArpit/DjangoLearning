29486cbd6953c5babf5bc0748fa1e5c78458dbf6
Arpit <arpit.gangwar061@gmail.com>
2025-12-04 18:29:00 +0530

    Updated 4 files on 2025-12-04 18:29:00

--- my_site/authenticationn/views.py
+++ my_site/authenticationn/views.py
@@ -5,6 +5,6 @@ def home(request):
     return HttpResponse("Welcome to the authentication home page!")
 
 def login(request):
-    return render(request, 'index.html')
+    return render(request, 'login.html')
 
 # Create your views here.
