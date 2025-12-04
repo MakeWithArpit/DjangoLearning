1977777c7db8f3bf8e839249d409535d5f9a7679
Arpit <arpit.gangwar061@gmail.com>
2025-12-04 18:32:06 +0530

    Modified: y_site/authenticationn/__pycache__/views.cpython-313.pyc, my_site/authenticationn/views.py, my_site/authenticationn/templates/login1.html

--- my_site/authenticationn/views.py
+++ my_site/authenticationn/views.py
@@ -5,6 +5,5 @@ def home(request):
     return HttpResponse("Welcome to the authentication home page!")
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login1.html')
 
-# Create your views here.
