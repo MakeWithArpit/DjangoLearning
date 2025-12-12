c09b0759b7b5065f432d6b04b2cd62a67bbd4304
Arpit <arpit.gangwar061@gmail.com>
2025-12-12 14:41:18 +0530

    Updated 5 files on 2025-12-12 14:41:17

--- my_site/authenticationn/views.py
+++ my_site/authenticationn/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def auth_home(request):
-    data = {
-        "title" : "Authentication Home",
+    # data = {
+    #     "title" : "Authentication Home",
         
-        "course_list" : ["Django", "Flask", "FastAPI", "Tornado"],
+    #     "course_list" : ["Django", "Flask", "FastAPI", "Tornado"],
 
-        "student_list" : [
-            {"name": "Alice", "age": 24},
-            {"name": "Bob", "age": 22},
-            {"name": "Charlie", "age": 23},
-        ],
+    #     "student_list" : [
+    #         {"name": "Alice", "age": 24},
+    #         {"name": "Bob", "age": 22},
+    #         {"name": "Charlie", "age": 23},
+    #     ],
 
-    }
-    return render(request, 'index.html', data)
+    # }
+    return render(request, 'home/index.html')
 
 def auth_login(request):
     return render(request, 'login_page/login.html')
