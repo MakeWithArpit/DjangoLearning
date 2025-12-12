fe909a7e88e743de3c09cd7de4a496ead0ce344a
Arpit <arpit.gangwar061@gmail.com>
2025-12-12 21:10:38 +0530

    Updated 4 files on 2025-12-12 21:10:38

--- my_site/authenticationn/views.py
+++ my_site/authenticationn/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def auth_home(request):
+def home(request):
     return render(request, 'index.html')
 
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'signup.html')
+
+def forgot(request):
+    return render(request, 'forgot-password.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from authenticationn.views import *
 
 urlpatterns = [
-    path('', auth_home, name='auth_home'),
+    path('', home, name='home'),
+    path('', login, name='login'),
+    path('', register, name='register'),
+    path('', forgot, name='forgot'),
+    path('', contact, name='contact'),
+    path('', about, name='about'),
+
     path('admin/', admin.site.urls),
 ]
