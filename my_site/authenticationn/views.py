8704233e05cbc03bb0c35e970016cc4a2cda878d
Arpit <arpit.gangwar061@gmail.com>
2025-12-13 22:14:56 +0530

    Modified: y_site/authenticationn/__pycache__/views.cpython-313.pyc, my_site/authenticationn/views.py

--- my_site/authenticationn/views.py
+++ my_site/authenticationn/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, "login.html")
+
 
 def register(request):
-    return render(request, 'signup.html')
+    return render(request, "signup.html")
+
 
 def forgot(request):
-    return render(request, 'forgot-password.html')
+    return render(request, "forgot-password.html")
+
 
 def contact(request):
-    try:
-        print(f"name    :    {request.GET['name']}")
-        print(f"Email   :    {request.GET['email']}")
-        print(f"Message :    {request.GET['message']}")
-    except:
-        pass    
-    return render(request, 'contact.html')
+    if request.method == "GET":
+        name = request.GET.get("name")
+        email = request.GET.get("email")
+        message = request.GET.get("message")
+
+        if name and email and message:
+            print("Name    :", name)
+            print("Email   :", email)
+            print("Message :", message)
+        print("Contact form submitted.")
+    return render(request, "contact.html")
+
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
